4ec45d6170417ed2714a31ece77c73fc572ff4cc
Sam Ashtar <samashtarm@gmail.com>
2018-08-25 06:06:01 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |k, v|
+    v.each do |k2, v2|
+      v2.each do |i|
+        pigeon_list[i] ||= {}
+        pigeon_list[i][k] ||= []
+        pigeon_list[i][k] << k2.to_s
+      end
+    end
+  end
+pigeon_list
 end
